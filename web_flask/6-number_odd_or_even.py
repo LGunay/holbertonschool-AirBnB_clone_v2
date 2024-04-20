f8913a07c809bdc79a48530a71a5cd81a77d5f2e
gunay <gqasimova93@gmail.com>
2024-04-20 20:39:07 +0400

    odd or even

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -1,45 +1,45 @@
 #!/usr/bin/python3
-"""starts a Flask web application"""
-from flask import Flask
-from flask import render_template
+"""HBNB"""
 
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route('/', strict_slashes=False)
-def index():
+@app.route("/", strict_slashes=False)
+def home():
     return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def display_text(text):
-    return 'C {}'.format(text.replace('_', ' '))
+@app.route("/c/<text>", strict_slashes=False)
+def C(text):
+    return f"C {text}".replace("_", " ")
 
 
-@app.route('/python', strict_slashes=False, defaults={'text': 'is cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def display_python(text):
-    return 'Python {}'.format(text.replace('_', ' '))
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python(text="is cool"):
+    return f"Python {text}".replace("_", " ")
 
 
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_int(n):
-    return '{}  is a number'.format(n)
+@app.route("/number/<int:n>", strict_slashes=False)
+def number(n):
+    return f"{n} is a number"
 
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def display_template(n):
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>")
+def n_temp(n):
+    return render_template("5-number.html", n=n)
 
 
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def odd_or_even(n):
-    return render_template('6-number_odd_or_even.html', n=n)
+@app.route("/number_odd_or_even/<int:n>")
+def odd_even(n):
+    return render_template("6-number_odd_or_even.html", n=n)
 
 
 if __name__ == "__main__":
