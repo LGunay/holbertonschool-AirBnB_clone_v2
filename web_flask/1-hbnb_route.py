141a0ac43e3d8df12b8372fccf384d9e5db57ebd
gunay <gqasimova93@gmail.com>
2024-04-20 18:31:32 +0400

    fixed document

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Hello Flask!"""
+"""HBNB"""
 
 from flask import Flask
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def home():
     return "Hello HBNB!"
 
 
-@APP.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     return "HBNB"
 
