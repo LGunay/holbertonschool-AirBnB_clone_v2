63978eab28e93824eb55bad4712f8058e400c398
gunay <gqasimova93@gmail.com>
2024-04-21 18:24:51 +0400

    list of state

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
+"""list of state"""
 
-'''A simple Flask web application.
-'''
+
 from flask import Flask, render_template
-
-from models import storage
 from models.state import State
-
+from models import storage
 
 app = Flask(__name__)
-'''The Flask application instance.'''
 app.url_map.strict_slashes = False
 
 
-@app.route('/states_list')
-def states_list():
-    '''The states_list page.'''
-    all_states = list(storage.all(State).values())
-    all_states.sort(key=lambda x: x.name)
-    ctxt = {
-        'states': all_states
-    }
-    return render_template('7-states_list.html', **ctxt)
+@app.route("/states_list")
+def lists():
+    storage_new = []
+    for states in storage.all(State).values():
+        storage_new.append(states.sort())
+    return render_template("7-states_list.html", states=storage_new)
 
 
 @app.teardown_appcontext
-def flask_teardown(exc):
-    '''The Flask app/request context end event listener.'''
+def close(exception):
     storage.close()
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
